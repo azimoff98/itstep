50916db9fc913ff0d214c5fbcb4570c83b45e4e0
azimoff98 <azimoff.q@gmail.com>
2018-05-24 23:59:25 +0400

    second commit for trello beta

--- src/main/java/az/itstep/azjava/testapp/Application.java
+++ src/main/java/az/itstep/azjava/testapp/Application.java
@@ -7,5 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+
+        /*
+        testing all commits for trello beta 
+
+         */
     }
 }
